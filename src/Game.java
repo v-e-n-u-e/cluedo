9d2a4b8b46210750ddc08f695366ae688b28ef4b
v-e-n-u-e <cinnamanswirls@gmail.com>
2016-08-03 19:02:17 +1200

    changed if statements

--- src/Game.java
+++ src/Game.java
@@ -235,9 +235,9 @@ public class Game {
 	
 			if(p.holds(guess.getRoom())){//If the player holds the room card
 				System.out.println(p.getName() + "holds:"+guess.getRoom().getName());
-			}else if(p.holds(guess.getMurderer())){//If the player holds the suspect card
+			}if(p.holds(guess.getMurderer())){//If the player holds the suspect card
 				System.out.println(p.getName() + "holds:"+guess.getMurderer().getName());
-			}else if(p.holds(guess.getWeapon())){//If the player holds the weapon card
+			}if(p.holds(guess.getWeapon())){//If the player holds the weapon card
 				System.out.println(p.getName() + "holds:"+guess.getWeapon().getName());
 			}
 			
