9c31f6ecb33891f96b091f8138fc7c5e7db3320a
v-e-n-u-e <cinnamanswirls@gmail.com>
2016-08-04 15:54:34 +1200

    updated txt, added leave, fixed enter

--- src/Room.java
+++ src/Room.java
@@ -1,12 +1,51 @@
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Room implements Tile {
-
+	ArrayList<Point> doors;
 	Point pos;
 	String name;
-	public Room(String name,Point pos){
+	public Room(String name,Point pos){//sets up an arraylist of doors that the room contains. used to let players leave onto them
 		this.name=name;
+		doors = new ArrayList<Point>();
 		this.pos=pos;
+	/*	if(this.print()=="K"){
+			doors.add(new Point(4,6));
+		}
+		else if(this.print()=="d"){
+			doors.add(new Point(7,12));
+			doors.add(new Point(6,15));
+		}
+		else if(this.print()=="l"){
+			doors.add(new Point(6,19));
+		}
+		else if(this.print()=="B"){
+			doors.add(new Point(8,5));
+			doors.add(new Point(9,7));
+			doors.add(new Point(15,7));
+			doors.add(new Point(16,5));
+		}
+		else if(this.print()=="H"){
+			doors.add(new Point(11,18));
+			doors.add(new Point(12,18));
+			doors.add(new Point(13,18));
+			doors.add(new Point(15,20));
+		}
+		else if(this.print()=="C"){
+			doors.add(new Point(19,4));
+		}
+		else if(this.print()=="b"){
+			doors.add(new Point(19,9));
+			doors.add(new Point(23,12));
+		}
+		else if(this.print()=="L"){
+			doors.add(new Point(21,14));
+			doors.add(new Point(18,16));
+		}
+		else if(this.print()=="S"){
+			doors.add(new Point(18,21));
+		}
+	}*/
 	}
 	
 	
